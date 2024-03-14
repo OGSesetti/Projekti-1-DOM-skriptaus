84b36263e23ea354ac75093a8b712c03e49f42d9
OGSesetti <91277863+OGSesetti@users.noreply.github.com>
2024-03-14 13:12:52 +0200

    stuff add
    added stuff

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function addTask() {
+    var taskInput = document.getElementById("taskInput");
+    var inputValue = taskInput.value;
 
+    if (inputValue === "") {
+        alert("Syötä tekstiä lisätäksesi tehtävän.")
+    }
+    var li = document.createElement("li");
+    li.textcontent = inputValue;
+    document.getElementById("taskList").appendChild(li);
+    taskInput.value = "";
+}
