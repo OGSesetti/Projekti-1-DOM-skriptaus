8bd95425ad4976d6d5fee3f8e057e368bcc22a9a
OGSesetti <91277863+OGSesetti@users.noreply.github.com>
2024-03-14 20:01:12 +0200

    jee

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+/*const itemsArray = localStorage.getItem("")
+
+
+
+
 function addTask() {
     var textInput = document.getElementById("textInput");
     var inputValue = textInput.value;
@@ -10,6 +15,24 @@ function addTask() {
     document.getElementById("taskList").appendChild(li);
     textInput.value = "";
 }
-/*function addTask() {
-    alert("What's up guys my name is Dirk Tendick")
-}*/
+*/
+const itemsArray = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem)("tasks") : null;
+console.log(itemsArray);
+//Kirjoitettu tehtävä lisätään listaan ja local storageen
+function addTask() {
+    let textInput = document.getElementById("textInput");
+    let inputValue = textInput.value;
+    if (inputValue === "") {
+        alert("Syötä tekstiä lisätäksesi tehtävän.")
+    }
+    itemsArray.push(item.value);
+}
+
+//localStorageen tallennetut tehtävät lisätään takaisin listaan
+function taskLoader() {
+    for (i = 0, i = data.length, i++);
+    {
+        get
+    }
+
+}
