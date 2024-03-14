1271d968bb3c05f1e74ff11efdecf4c9d0572ae4
OGSesetti <91277863+OGSesetti@users.noreply.github.com>
2024-03-15 01:09:35 +0200

    jeee

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-/*const itemsArray = localStorage.getItem("")
-
+/*
+Variablet ja muut nimet:
+textInput = viimeksi lisätyn tekstin sisältö
+tasksArray = tehtävälistamuuttuja
+tasks = avain tehtävien löytämiseen
+taskContainer = div, mihin tehtävät menee
+checkButton = nappi, jolla tehtävä merkataan tehdyksi
 
 
 
@@ -16,23 +21,44 @@ function addTask() {
     textInput.value = "";
 }
 */
-const itemsArray = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem)("tasks") : null;
+/* const itemsArray = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem)("tasks") : null;
 console.log(itemsArray);
+document.querySelector = ("#enter").addeventListener("click.")(); {
+    const item = document.querySelector("#item")
+    createItem(item)
+}
+*/
+
 //Kirjoitettu tehtävä lisätään listaan ja local storageen
+
+const tasksArray = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem)("tasks") : null;
+
 function addTask() {
     let textInput = document.getElementById("textInput");
-    let inputValue = textInput.value;
-    if (inputValue === "") {
+    if (textInput == "") {
         alert("Syötä tekstiä lisätäksesi tehtävän.")
     }
-    itemsArray.push(item.value);
+    else {
+        elementCreator(task)
+
+    }
 }
 
 //localStorageen tallennetut tehtävät lisätään takaisin listaan
 function taskLoader() {
-    for (i = 0, i = data.length, i++);
-    {
-        get
+    if (itemsArray != null) {
+        for (i = 0; i <= itemsArray.length; i++) {
+            let key = "tasks" + i;
+            let task = localStorage.getItem(key);
+            elementCreator(task)
+        }
     }
+}
+
+function elementCreator(task) {
+    const listLocation = document.getElementById("listContainer")
+    let taskDiv = document.createElement("div");
+    taskDiv.textContent = task;
+    listLocation.appendChild(taskDiv);
 
 }
