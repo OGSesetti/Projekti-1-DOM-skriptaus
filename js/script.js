38bc5544ae7a98d70c74abf7a2949babfc42009c
OGSesetti <91277863+OGSesetti@users.noreply.github.com>
2024-03-15 13:11:33 +0200

    jeeeeeeee

--- js/script.js
+++ js/script.js
@@ -1,37 +1,19 @@
 /*
 Variablet ja muut nimet:
+
 textInput = viimeksi lisätyn tekstin sisältö
 tasksArray = tehtävälistamuuttuja
 tasks = avain tehtävien löytämiseen
 taskContainer = div, mihin tehtävät menee
 checkButton = nappi, jolla tehtävä merkataan tehdyksi
-
-
-
-function addTask() {
-    var textInput = document.getElementById("textInput");
-    var inputValue = textInput.value;
-
-    if (inputValue === "") {
-        alert("Syötä tekstiä lisätäksesi tehtävän.")
-    }
-    var li = document.createElement("li");
-    li.textcontent = inputValue;
-    document.getElementById("taskList").appendChild(li);
-    textInput.value = "";
-}
-*/
-/* const itemsArray = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem)("tasks") : null;
-console.log(itemsArray);
-document.querySelector = ("#enter").addeventListener("click.")(); {
-    const item = document.querySelector("#item")
-    createItem(item)
-}
+nuke = funktio joak tyhjentää kaiken
+clearCompleted = funktio, joka tyhjentää valmiiksi merkityt tehtävät
 */
 
 //Kirjoitettu tehtävä lisätään listaan ja local storageen
 const storedTasks = localStorage.getItem("tasks");
 const tasksArray = storedTasks ? JSON.parse(storedTasks) : [];
+const listLocation = document.getElementById("listContainer")
 //const tasksArray = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem)("tasks") : [];
 
 console.log(tasksArray);
@@ -55,16 +37,16 @@ function addTask() {
 //localStorageen tallennetut tehtävät lisätään takaisin listaan
 function taskLoader() {
     if (tasksArray != null) {
-        for (i = 0; i < tasksArray.length; i++) {
-            let key = "tasks" + i;
-            let task = localStorage.getItem(key);
+        for (let i = 0; i < tasksArray.length; i++) {
+            let task = tasksArray[i];
+            //           let key = "tasks" + i;
+            //           let task = localStorage.getItem(key);
             elementCreator(task)
         }
     }
 }
 
 function elementCreator(task) {
-    const listLocation = document.getElementById("listContainer")
     let taskDiv = document.createElement("div");
     taskDiv.textContent = task;
     listLocation.appendChild(taskDiv);
@@ -72,5 +54,12 @@ function elementCreator(task) {
 }
 
 function nuke() {
-    tasksArray.length = 0;
+    localStorage.removeItem("tasks");
+    tasksArray.splice(0, tasksArray.length);
+    listLocation.innerHTML = "";
+}
+
+function clearCompleted() {
+
+
 }
